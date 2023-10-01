0a76445040d7d58715e17464087ec5a6c648804f
viji9898 <vijitha.wijesuriya@gmail.com>
2023-10-01 14:46:43 +0530

    activate

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { NotFound } from "./utils/notFound";
-// import { NavBar } from "./components/navigation/navBar";
+import { NavBar } from "./components/navigation/navBar";
 
 // import ScrollToTop from "./utils/ScrollToTop";
 
 import { Assets } from "./pages/Assets";
-// import { WorkShop } from "./pages/Workshop";
-import { Deactivated } from "./pages/deactiviated";
+import { WorkShop } from "./pages/Workshop";
+// import { Deactivated } from "./pages/deactiviated";
 // import { ProgramList } from "./components/landingPage/programList";
 
 function App() {
   return (
     <Router>
-      {/* <NavBar /> */}
+      <NavBar />
       {/* <ScrollToTop /> */}
       <Routes>
         {/* <Route exact path="/" element={<ProgramList />} /> */}
-        {/* <Route exact path="/" element={<WorkShop />} /> */}
-        <Route exact path="/" element={<Deactivated />} />
+        <Route exact path="/" element={<WorkShop />} />
+        {/* <Route exact path="/" element={<Deactivated />} /> */}
         <Route exact path="/assets" element={<Assets />} />
         <Route exact path="/*" element={<NotFound />} />
       </Routes>
