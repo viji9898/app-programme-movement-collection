5c947dc98ec31e8b9dbfca35595ec399f14846e3
viji9898 <vijitha.wijesuriya@gmail.com>
2023-10-04 13:08:01 +0530

    update content

--- src/components/workshopData/eoMobilityData.js
+++ src/components/workshopData/eoMobilityData.js
@@ -1,6 +1,6 @@
 export const eoMobilityData = [
   {
-    title: "6010",
+    title: "90/90",
     color: "volcano",
     grouping: "1a",
     sets: "3",
@@ -25,12 +25,12 @@ export const eoMobilityData = [
     id: "scapular-shrugs-push",
   },
   {
-    title: "6009 ",
+    title: "Long Lunge",
     id: "hollow-body",
     color: "volcano",
     grouping: "1b",
     sets: "3",
-    reps: "30 secs",
+    reps: "30 secs hold each side",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/d600ae60-10a1-4478-89ad-272af4262f03.gif?ClientID=vimeo-core-prod&Date=1696390992&Signature=1f5103569c7c200d1cd2e5dd358d22f96758bdc0",
@@ -49,11 +49,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912190/14314239ef?share=copy",
   },
   {
-    title: "6008",
+    title: "Single Leg Frog",
     color: "volcano",
     grouping: "1c",
     sets: "3",
-    reps: "10",
+    reps: "8 each side",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/e4b45ae8-2334-4345-89f9-bdeb9d5154a1.gif?ClientID=vimeo-core-prod&Date=1696391212&Signature=0f3c70b9a62979c5832a3b21037cda89efb4d436",
@@ -81,11 +81,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912218/1fd1375f06?share=copy",
   },
   {
-    title: "6007",
+    title: "Horse Squat",
     color: "volcano",
     grouping: "1d",
     sets: "3",
-    reps: "20 secs",
+    reps: "30 secs",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/134f9583-0d98-462f-a1ee-cb8c86202810.gif?ClientID=vimeo-core-prod&Date=1696391681&Signature=adb05b242e7950a7b4d7f5ef603d518075ce6890",
@@ -94,11 +94,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912240/0043df2f50?share=copy",
   },
   {
-    title: "6006",
+    title: "Frog Rocks",
     color: "volcano",
     grouping: "1e",
     sets: "3",
-    reps: "10",
+    reps: "60 seconds",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/b1cab29c-6f1d-4a37-88a4-15f912410036.gif?ClientID=vimeo-core-prod&Date=1696391705&Signature=bafefc681df4ceb60273b60167983dc8a1a84d70",
@@ -107,11 +107,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912268/d351073c30?share=copy",
   },
   {
-    title: "6005",
+    title: "Glute Bridge",
     color: "volcano",
     grouping: "1f",
     sets: "3",
-    reps: "10",
+    reps: "15",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/5aabcdfd-78bc-4a30-8903-4870028cd2b2.gif?ClientID=vimeo-core-prod&Date=1696391755&Signature=3627ed6305ef2cea3ecf5377e1b06c72fd099115",
@@ -120,11 +120,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912304/dd01a703fe?share=copy",
   },
   {
-    title: "6004",
+    title: "Squat Lift",
     color: "orange",
     grouping: "2",
     sets: "3",
-    reps: "1-3 reps",
+    reps: "8",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/c7f842c4-ff82-4f3e-b473-ee1daed08eee.gif?ClientID=vimeo-core-prod&Date=1696391776&Signature=3cf3f10b9b4d779128771920a232d540d0fb1434",
@@ -133,11 +133,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912328/a930a71e1c?share=copy",
   },
   {
-    title: "6003",
+    title: "Seated Hip Swivels",
     color: "gold",
     grouping: "3",
     sets: "3",
-    reps: "10-30 seconds",
+    reps: "10 each side",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/6251fded-997d-439e-866a-7e131b40f2ca.gif?ClientID=vimeo-core-prod&Date=1696391823&Signature=afd7bb848928992c106316e8601ceccfec657ece",
@@ -146,11 +146,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912361/ccf9656c28?share=copy",
   },
   {
-    title: "6002",
+    title: "Twisting Bear",
     color: "blue",
     grouping: "4",
     sets: "3",
-    reps: "10-20secs",
+    reps: "10 each side",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/425926d0-f4ba-4447-96e9-2857b36197ad.gif?ClientID=vimeo-core-prod&Date=1696391918&Signature=538c91960efb84ca8cbc37094294455459ff7e85",
@@ -163,11 +163,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912390/101cddf8df?share=copy",
   },
   {
-    title: "6001",
+    title: "Plank Hold",
     color: "purple",
     grouping: "5a",
     sets: "3",
-    reps: "10",
+    reps: "45 secoonds hold",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/16604bfb-d8b0-45a0-b95f-31cc27e69e27.gif?ClientID=vimeo-core-prod&Date=1696392400&Signature=5d4bb8eedd8a406ba664e77089c63a0150328870",
@@ -176,7 +176,7 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912405/971e70892a?share=copy",
   },
   {
-    title: "5999",
+    title: "Superman",
     color: "purple",
     grouping: "5b",
     sets: "3",
@@ -189,11 +189,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912431/e491f03e13?share=copy",
   },
   {
-    title: "5998",
+    title: "Scapula Shrugs",
     color: "cyan",
     grouping: "6a",
     sets: "3",
-    reps: "5-8 reps",
+    reps: "10",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/8e2d0c5c-2a5a-4875-b51e-19f6fc32001d.gif?ClientID=vimeo-core-prod&Date=1696392494&Signature=81426dd1c3181c1d0c5cd5ad5d0ffea18995d83f",
@@ -203,7 +203,7 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912484/7e9e9a268f?share=copy",
   },
   {
-    title: "5997",
+    title: "Hollow Body Hold",
     color: "cyan",
     grouping: "6b",
     sets: "3",
@@ -216,11 +216,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912527/807b4bf40a?share=copy",
   },
   {
-    title: "5996",
+    title: "Reverse Table Top Taps",
     color: "cyan",
     grouping: "6b",
     sets: "3",
-    reps: "30secs",
+    reps: "8",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/823fa7c7-60d7-4d6c-843f-c2dc65fd35af.gif?ClientID=vimeo-core-prod&Date=1696392633&Signature=86482cf4c86bc113482707fef5d1020584e8cc40",
@@ -229,11 +229,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912559/d946be1cf4?share=copy",
   },
   {
-    title: "5995",
+    title: "Scorpion",
     color: "cyan",
     grouping: "6b",
     sets: "3",
-    reps: "30secs",
+    reps: "4 each side",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/4894cf48-518c-4d2d-97bd-31394914232c.gif?ClientID=vimeo-core-prod&Date=1696392666&Signature=dcf08f2add6a9043a177e9d95b7f69465b5014a2",
@@ -245,11 +245,11 @@ export const eoMobilityData = [
     videoUrl: "https://vimeo.com/870912585/b019135564?share=copy",
   },
   {
-    title: "5994",
+    title: "Inch Work",
     color: "cyan",
     grouping: "6b",
     sets: "3",
-    reps: "30secs",
+    reps: "8",
     gifUrl: [
       {
         url: "https://videoapi-muybridge.vimeocdn.com/animated-thumbnails/image/49e5cee2-89d9-4091-be1a-88c972629a7a.gif?ClientID=vimeo-core-prod&Date=1696392685&Signature=9285bb5318110e31415a2a3e814e93f56572ad77",
